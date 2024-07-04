f3de5cbd6f961156495957a3952d2d4f50830d4b
Ephriam Kassa <ephriam123@gmail.com>
2024-07-04 14:15:09 -0700

    sdasccascassdfrf

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const LandingPage = () => {
   const images = [
     './ton.png',
-    './not.jpg',
+    './not.png',
     './ton.png',
+    './not.png',
     './ton.png',
+    './not.png',
     './ton.png',
-    './ton.png',
-    './ton.png',
-    './ton.png',
+    './not.png',
     './ton.png',
   ];
 
